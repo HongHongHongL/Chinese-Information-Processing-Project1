c298d9ceb3803c1a3e5d364e87b7b050c5e1a968
SUSTechHong <11612425@mail.sustech.edu.cn>
2019-10-06 17:06:24 +0800

    增加参数配制

--- translator.py
+++ translator.py
@@ -1,4 +1,5 @@
 import csv
+import codecs
 from read import *
 
 TTSMap = dict()
@@ -24,6 +25,7 @@ def Traditional_To_Simplified(txt):
                 i += 1
     return ret
 
+
 def Simplified_To_Traditional(txt):
     ret = ""
     i = 0
@@ -38,7 +40,39 @@ def Simplified_To_Traditional(txt):
                 i += 1
     return ret
 
+
 if __name__ == '__main__':
     Get_Config()
-    print(Traditional_To_Simplified("天好藍要和你一起看，起風時由你来温暖。"))
-    print(Simplified_To_Traditional("天好蓝要和你一起看，起风时由你来温暖。"))
+    import sys
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-e', type='string', dest='encoding',
+                      help='encoding')
+    parser.add_option('-f', type='string', dest='file_in',
+                      help='input file (- for stdin)')
+    parser.add_option('-t', type='string', dest='file_out',
+                      help='output file')
+    (options, args) = parser.parse_args()
+    if not options.encoding:
+        parser.error('encoding must be set')
+    if options.file_in:
+        if options.file_in == '-':
+            file_in = sys.stdin
+        else:
+            file_in = codecs.open(options.file_in, 'r+', encoding='utf-8')
+    else:
+        file_in = sys.stdin
+    if options.file_out:
+        if options.file_out == '-':
+            file_out = sys.stdout
+        else:
+            file_out = codecs.open(options.file_out, 'wb', encoding='utf-8')
+    else:
+        file_out = sys.stdout
+
+    if options.encoding == "STT":
+        for line in file_in:
+            file_out.write(Simplified_To_Traditional(line))
+    else:
+        for line in file_in:
+            file_out.write(Traditional_To_Simplified(line))

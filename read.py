fed315a1544edd6e346e520f3ace397cd9e1c1bf
Dararara <36438137+Dararara@users.noreply.github.com>
2019-10-06 16:31:08 +0800

    Update read.py

--- read.py
+++ read.py
@@ -3,18 +3,16 @@ import csv
 def tradition_to_simplify():
     ch_dict = dict()
     with open('data/zh2Hans.csv', 'r', newline='', encoding='utf-8') as infile:
-        reader = csv.DictReader(infile, delimiter = ';')
+        reader = csv.DictReader(infile)
         for row in reader: 
             ch_dict[row['繁体']] = row['简体']
-    #print(li_of_dicts)
     return ch_dict
 
 def simplify_to_tradition():
     ch_dict = dict()
     with open('data/zh2Hant.csv', 'r', newline='', encoding='utf-8') as infile:
-        reader = csv.DictReader(infile, delimiter = ';')
+        reader = csv.DictReader(infile)
         for row in reader: 
             ch_dict[row['简体']] = row['繁体']
-    #print(li_of_dicts)
+    #print(ch_dict)
     return ch_dict
-    
